9cc3e47e56dc6e16d269521f936ce3305b5de70c
Andre Ribeiro Martins Marques Mourato <andremourato@av.it.pt>
2019-11-28 06:53:06 +0000

    Phong Lighting and moving light source

--- src/assets/js/Entity.js
+++ src/assets/js/Entity.js
@@ -22,23 +22,8 @@ class Entity {
         this.ibo = gl.createBuffer()
         this.tbo = gl.createBuffer()
 
-        // Computing the Model-View Matrix
-        console.log(this.tx,this.ty,this.tz)
-        mat4.translate(this.world,this.world,vec4.fromValues(this.tx,this.ty,this.tz))
-        //Rotate on the X Axis
-        mat4.rotate(this.world,this.world,
-            glMatrix.toRadian(this.angleXX),
-            vec4.fromValues(1,0,0))
-        //Rotate on the Y Axis
-        mat4.rotate(this.world,this.world,
-            glMatrix.toRadian(this.angleYY),
-            vec4.fromValues(0,1,0))
+        this.updatePosition()
         
-        //Rotate on the Z Axis
-        mat4.rotate(this.world,this.world,
-            glMatrix.toRadian(this.angleZZ),
-            vec4.fromValues(0,0,1))
-
         // //Texture
         gl.bindBuffer(gl.ARRAY_BUFFER, this.tbo);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
@@ -54,5 +39,22 @@ class Entity {
     
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    }
+
+    updatePosition(){
+        mat4.translate(this.world,this.world,vec4.fromValues(this.tx,this.ty,this.tz))
+        //Rotate on the X Axis
+        mat4.rotate(this.world,this.world,
+            glMatrix.toRadian(this.angleXX),
+            vec4.fromValues(1,0,0))
+        //Rotate on the Y Axis
+        mat4.rotate(this.world,this.world,
+            glMatrix.toRadian(this.angleYY),
+            vec4.fromValues(0,1,0))
+        
+        //Rotate on the Z Axis
+        mat4.rotate(this.world,this.world,
+            glMatrix.toRadian(this.angleZZ),
+            vec4.fromValues(0,0,1))
     }
 }
